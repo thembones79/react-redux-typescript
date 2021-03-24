24aeb83781dddc3a88604bb3e821b43bfb1df389
thembones79 <michal.szumnarski@gmail.com>
2021-03-24 07:50:30 +0100

    mapstatetoprops refactor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -55,12 +55,12 @@ class _App extends React.Component<AppProps, AppState> {
   }
 }
 
-//const mapStateToProps = (state: StoreState): {todos: Todo[]} => {
-//  return { todos: state.todos };
-//};
-
-const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
-  return { todos };
+const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
+  return { todos: state.todos };
 };
+
+//const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+//  return { todos };
+//};
 
 export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
